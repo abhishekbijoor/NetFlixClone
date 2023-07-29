cc8e15947ddf8842cb45920290e39b24f842dc4d
Abhishek Bijoor <abhishek.bijoor@ibm.com>
2023-07-29 16:18:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.scss';
+import Home from './Pages/Home/Home';
+import Register from './Pages/Register/Register';
+import Watch from './Pages/watch/Watch';
+function App() {
+  return (
+    <Register/>
+  );
+}
+
+export default App;
